f56b6aad868a997a33c278662d107aab5b99ef66
Ilchenko_Grigorii_QA <ilchenkogr3865@gmail.com>
2022-11-29 03:27:22 +0300

    First commit

---
+++ cypress/integration/1-My-tests/staya.js
@@ -0,0 +1,25 @@
+describe('Проверка авторизации', function () {
+    it('Верная почта + верный пароль', function () {
+        cy.visit ('https://staya.dog/');
+        cy.get('.header-bottom__right--link').click();
+        cy.get('.auth-form > form > [type="email"]').type('ilchenkogr3865@gmail.com');
+        cy.get('.auth-form__submit').should('be.disabled');
+        cy.get('.auth-form > form > [type="password"]').type('cyTestn1');
+        cy.get('.auth-form__submit').should('not.be.disabled');
+        cy.get('.auth-form__submit').click();
+        cy.contains('Мои заказы');
+    })
+    it('Верная почта + НЕверный пароль', function () {
+        cy.visit ('https://staya.dog/');
+        cy.get('.header-bottom__right--link').click();
+        cy.get('button.profile__nav-link').click();
+        cy.get('.box__button_ok').click();
+        cy.get('.header-bottom__right--link').click();
+        cy.get('.auth-form > form > [type="email"]').type('ilchenkogr3865@gmail.com');
+        cy.get('.auth-form__submit').should('be.disabled');
+        cy.get('.auth-form > form > [type="password"]').type('cyTestn2');
+        cy.get('.auth-form__submit').should('not.be.disabled');
+        cy.get('.auth-form__submit').click();
+        cy.contains('Невозможно войти с предоставленными учетными данными.');
+    })
+})
